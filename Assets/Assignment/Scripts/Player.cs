77a92bd844def4ba506300fa441608ab4ade8fa0
Chase <rosschas@sheridancollege.ca>
2024-04-02 20:53:35 -0400

    Made the player character move.

--- Assets/Assignment/Scripts/Player.cs
+++ Assets/Assignment/Scripts/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
+    public Rigidbody2D rb;
+
+    public float speed = 5f;
+
+    Vector2 move;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * speed * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
     }
 }
